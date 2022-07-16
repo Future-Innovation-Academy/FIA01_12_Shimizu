e73a02cc96c664e654618d7bbf1aca151f98e81c
y-shimizu-12 <108227625+y-shimizu-12@users.noreply.github.com>
2022-07-16 10:48:04 +0900

    feat: 課題01を提出

---
+++ 01_janken_0619/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import gu from 'https://2.bp.blogspot.com/-VhlO-Yfjy_E/Uab3z3RNJQI/AAAAAAAAUVg/fX8VnSVDlWs/s800/janken_gu.png'
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [choice, setChoice] = useState()
+  const [hand, setHand] = useState()
+  const [result, setResult] = useState()
+
+  function janken(hand) {
+    const enemyHand = getRandomInt(3)
+    const gap = hand - enemyHand
+    if (gap === 0) {
+      setResult("DRAW")
+    } else if (gap == -1 | gap == 2) {
+      setResult("You WIN")
+    } else {
+      setResult("You LOSE")
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <button type="button" onClick={() => { setChoice(getRandomInt(3)), setHand(0), janken(0) }}>
+            gu
+          </button>
+          <button type="button" onClick={() => { setChoice(getRandomInt(3)), setHand(1), janken(1) }}>
+            choki
+          </button>
+          <button type="button" onClick={() => { setChoice(getRandomInt(3)), setHand(2), janken(2) }}>
+            pa
+          </button>
+        </p>
+        <p>
+          <img src={gu} alt="gu"/>
+        </p>
+        <h3>{result}</h3>
+      </header>
+    </div>
+  )
+}
+
+export default App
