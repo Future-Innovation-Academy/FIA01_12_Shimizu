ad597b7459842744c0fa3c33c30c563e759913b3
shimizu3494 <y.shimizu.bv@future.co.jp>
2022-07-17 16:11:06 +0900

    fix: vite用のコードに修正

--- 04_chat_0717/src/App.jsx
+++ 04_chat_0717/src/App.jsx
@@ -1,11 +1,100 @@
-import { useState, useEffect } from "react";
-import { collection, query, onSnapshot, addDoc } from "firebase/firestore";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { collection, query, onSnapshot, addDoc } from "firebase/firestore";
+import { db, auth } from "./firebase";
+import Add from "./Add";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // 1. useState
+  //useStateでデータを受け取れる準備をする
+  const [data, setData] = useState([{ id: "", title: "" }]);
+  console.log(data, "useStateの箱の方をみましょう！");
 
-  return <div className="App"></div>;
+  //3. 登録用のuseStateを準備します🤗
+  const [titleValue, setTitleValue] = useState("");
+
+  //2. useEffect
+  useEffect(() => {
+    //2.1 query = コレクション(firebaseの箱のこと)の指定をする
+    // firebaseで用意されたおまじない
+    const q = query(collection(db, "group")); //データにアクセス
+
+    //2.2
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      setData(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().title,
+        }))
+      );
+    });
+
+    return () => unsub();
+  }, []);
+
+  // inputのonChangeのイベントを記述🤗
+  const handleInputChange = (e) => {
+    // console.log(e, "event");
+    // console.log(e.target, "event target");
+    setTitleValue(e.target.value);
+  };
+
+  //送信の処理を記述＝送信のボタンが押されたら登録の処理を実行する🤗
+  const addData = async () => {
+    // 処理を記述していきます🤗
+    // alert(1); 記述後、送信ボタンを押す→画面に変化があればコメントアウトしましょう🤗
+
+    // firebaseへの登録の処理
+    await addDoc(
+      collection(db, "group"), //場所どこ？
+      {
+        title: titleValue,
+      }
+    );
+
+    // 文字を空にします🤗
+    setTitleValue("");
+  };
+
+  return (
+    <div className="App">
+      {/* 表示のロジックを記述 */}
+      <h1>データ表示</h1>
+
+      {/* 表示のロジックを記述していきます🤗 */}
+      {/* dataというuseStateで保持した箱の中にはfirebaseのデータが収納されています */}
+      {/* 収納されているデータをES6のmapというおまじないを使ってぐるぐる表示させます */}
+
+      {data.map((item, index) => (
+        // mapを使うときは,keyという指定が必須です！忘れるとエラーが出ます🤗
+        // Warning: Each child in a list should have a unique "key" prop.
+        <div key={index}>
+          <div>{index}数字</div>
+          <div>{item.id}</div>
+          <div>{item.title}</div>
+        </div>
+      ))}
+
+      {/* hrタグは線
+      <hr />
+      <h1>登録の処理</h1>
+      {/* このあとuseStateを新しく記述します🤗 */}
+      {/* <p>{titleValue}</p> */}
+
+      {/* 入力させるinputタグを記述 */}
+      {/* <input type="text" value={titleValue} onChange={handleInputChange} /> */}
+
+      {/* 送信のボタンを記述 */}
+      {/* <button onClick={addData}>送信</button> */}
+
+      {/* 中身をAdd にお引越し */}
+      <Add
+        addData={addData}
+        titleValue={titleValue}
+        handleInputChange={handleInputChange}
+      />
+    </div>
+  );
 }
 
 export default App;
--- 04_chat_0717/src/firebase.js
+++ 04_chat_0717/src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseApp = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 });
 
 //Firebase ver9 compliant (modular)
