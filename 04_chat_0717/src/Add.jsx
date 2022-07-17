f4eafeb138841d4faff0401c230fb82783211c67
shimizu3494 <y.shimizu.bv@future.co.jp>
2022-07-17 16:54:08 +0900

    fix: Add.jsxの入力部とボタン部をマテリアルデザインに変更

--- 04_chat_0717/src/Add.jsx
+++ 04_chat_0717/src/Add.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { TextField, Button } from "@mui/material";
 
 const Add = ({ addData, handleInputChange, titleValue }) => {
   return (
     <div>
       <h2>登録処理</h2>
       {/* このあとuseStateを新しく記述します🤗 */}
-      <p>{titleValue}</p>
+      {/* <p>{titleValue}</p> */}
 
       {/* 入力させるinputタグを記述 */}
-      <input type="text" value={titleValue} onChange={handleInputChange} />
+      {/* <input type="text" value={titleValue} onChange={handleInputChange} /> */}
+      <TextField
+        id="standard-basic"
+        label="追加したいテキスト"
+        variant="standard"
+        required
+        onChange={handleInputChange}
+        value={titleValue}
+      />
 
       {/* 送信のボタンを記述 */}
-      <button onClick={addData}>送信</button>
+      <Button variant="contained" onClick={addData}>
+        送信
+      </Button>
     </div>
   );
 };
