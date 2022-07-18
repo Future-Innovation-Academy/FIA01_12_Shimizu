bcc0e42fa7fa9224dacc9d53f1e69a8ae226a2b8
shimizu3494 <y.shimizu.bv@future.co.jp>
2022-07-18 14:39:08 +0900

    feat: webカメラで撮影中の画面をリアルタイム表示

--- 04_chat_0717/src/App.jsx
+++ 04_chat_0717/src/App.jsx
@@ -4,7 +4,56 @@ import { collection, query, onSnapshot, addDoc } from "firebase/firestore";
 import { db, auth } from "./firebase";
 import Add from "./Add";
 
+// import React from "react";
+import Webcam from "react-webcam";
+// import cv from "@techstark/opencv-js";
+// import { loadHaarFaceModels, detectHaarFace } from "./haarFaceDetection";
+
 function App() {
+  const [modelLoaded, setModelLoaded] = React.useState(false);
+
+
+  const webcamRef = React.useRef(null);
+  const imgRef = React.useRef(null);
+  const faceImgRef = React.useRef(null);
+
+  // React.useEffect(() => {
+  //   if (!modelLoaded) return;
+
+  //   const detectFace = async () => {
+  //     const imageSrc = webcamRef.current.getScreenshot();
+  //     if (!imageSrc) return;
+
+  //     return new Promise((resolve) => {
+  //       imgRef.current.src = imageSrc;
+  //       imgRef.current.onload = () => {
+  //         try {
+  //           const img = cv.imread(imgRef.current);
+  //           detectHaarFace(img);
+  //           cv.imshow(faceImgRef.current, img);
+
+  //           img.delete();
+  //           resolve();
+  //         } catch (error) {
+  //           console.log(error);
+  //           resolve();
+  //         }
+  //       };
+  //     });
+  //   };
+
+  //   let handle;
+  //   const nextTick = () => {
+  //     handle = requestAnimationFrame(async () => {
+  //       await detectFace();
+  //       nextTick();
+  //     });
+  //   };
+  //   nextTick();
+  //   return () => {
+  //     cancelAnimationFrame(handle);
+  //   };
+  // }, [modelLoaded]);
   // 1. useState
   //useStateでデータを受け取れる準備をする
   const [data, setData] = useState([{ id: "", title: "" }]);
@@ -58,6 +107,16 @@ function App() {
 
   return (
     <div className="App">
+      <h2>Real-time Face Detection</h2>
+      <Webcam
+        ref={webcamRef}
+        className="webcam"
+        mirrored
+        screenshotFormat="image/jpeg"
+      />
+      <img className="inputImage" alt="input" ref={imgRef} />
+      {/* <canvas className="outputImage" ref={faceImgRef} /> */}
+      {/* {!modelLoaded && <div>Loading Haar-cascade face model...</div>} */}
       <Add
         addData={addData}
         titleValue={titleValue}
