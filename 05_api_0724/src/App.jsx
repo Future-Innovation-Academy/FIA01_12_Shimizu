360534c927da32b543e30f294680b20d6f020a34
shimizu3494 <y.shimizu.bv@future.co.jp>
2022-07-30 14:49:18 +0900

    feat: firestoreに保存したapiKeyを取得する処理を追加

--- 05_api_0724/src/App.jsx
+++ 05_api_0724/src/App.jsx
@@ -1,7 +1,33 @@
 import "./App.css";
 import Data from "./components/Data";
+import React, { useState, useEffect } from "react";
+import { collection, query, onSnapshot, addDoc } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  // 1. useState
+  //useStateでデータを受け取れる準備をする
+  const [data, setData] = useState([{ id: "", apiKey: "" }]);
+  //2. useEffect
+  useEffect(() => {
+    //2.1 query = コレクション(firebaseの箱のこと)の指定をする
+    // firebaseで用意されたおまじない
+    const q = query(collection(db, "config")); //データにアクセス
+    //2.2
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      setData(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          apiKey: doc.data().apiKey,
+        }))
+      );
+    });
+    console.log({ data });
+    return () => unsub();
+  }, []);
+  console.log({ data });
+  console.log(data.at(0).apiKey);
+
   return (
     <div className="App">
       <Data />
